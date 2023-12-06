2b08e28b6a326fbb3db695a1401b7ab983e54264
MarinaASUS <marinabar1609@gmail.com>
2023-12-06 21:30:31 +0100

    fin TP8

--- TP8/8.6.py
+++ TP8/8.6.py
@@ -6,22 +6,50 @@ def genere(n, b):
     return l
 
 def enTableau(s):
-    return list(s)
+    return [int(x) for x in s]
 
 def copie(source):
     y=source[:]
-    print(id(y), id(source))
     return y
-
-print(enTableau("2432"))
 
 def verification(l1, l2):
     mp=0
     bp=0
+    l3=copie(l1)
+    l4=copie(l2)
     for i in range(len(l1)):
         if l1[i]==l2[i]:
             bp+=1
-        elif l2[i] in l1:
-            mp+=1
+            l3.pop(i)
+            l4.pop(i)
+
+    for i in range(len(l4)):
+        if l4[i] in l3:
+            if i!=l3.index(l4[i]):
+                mp+=1
+                l3.remove(l4[i])
+    print("Nombre d'élements bien placés : ", bp)
+    print("Nombre d'élements mal placés : ", mp)
     return bp, mp
-print(verification([1, 3, 2, 5], [3, 3, 2, 9]))
+
+
+def mastermind(n, b, nb_essais):
+    code=genere(n, b)
+    sol=False
+    ess=0
+    while not sol and ess!=nb_essais:
+        nombre=input(f'Saisir un nombre de {n} chiffres (compris entre 1 et {b}) : ')
+        nombre=enTableau(nombre)
+        bp, mp = verification(code, nombre)
+        if bp==n:
+            sol=True
+            print("Gagné")
+        ess+=1
+        if ess==nb_essais:
+            print('Perdu, dommage la solution était : ', code)
+        elif ess==nb_essais-1:
+            print("Dernier essai")
+        else:
+            print(f"Il vous reste {nb_essais - ess} essais")
+
+mastermind(5, 5, 5)
